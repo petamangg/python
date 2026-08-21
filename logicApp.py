04fda2f2ef76360bd6b030f89d296ce85425a8a8
Peter Tamamg <petamangg@gmail.com>
2026-08-21 12:37:33 +0000

    Logic App using if else

---
+++ logicApp.py
@@ -0,0 +1,7 @@
+#wap to input age and find out eligible for vote or not?
+age  = int (input("Enter the age : "))
+if age >= 18:
+    print("Eligible for vote")
+else:
+    print("Not eligible for vote")
+    
