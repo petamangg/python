5ebba4f2ef13fe5e06287ffafbed37d2c14c763b
Peter Tamamg <petamangg@gmail.com>
2026-08-21 13:04:46 +0000

    Odd Or Even Finder

---
+++ oddoreven.py
@@ -0,0 +1,6 @@
+#to find out wether the inputted number is odd or even
+number = int(input("Enter the number : "))
+if number % 2 == 0:
+    print("It is even number")
+else:
+    print("It is odd number")
